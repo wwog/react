046679f57eebcc950899cd29540716f960587883
wwog <871782513@qq.com>
2025-09-16 11:28:18 +0800

    feat: 更新 ArrayRender 组件，增加 renderEmpty 属性以处理空数组渲染

--- src/components/Struct/ArrayRender.tsx
+++ src/components/Struct/ArrayRender.tsx
@@ -5,16 +5,21 @@ export interface ArrayRenderProps<T> {
   items: T[];
   renderItem: (item: T, index: number) => React.ReactNode;
   filter?: (item: T) => boolean;
+  renderEmpty?: () => React.ReactNode;
 }
 //#endregion component Types
 
 //#region component
 export function ArrayRender<T>(props: ArrayRenderProps<T>): ReactNode {
-  const { items, renderItem, filter } = props;
+  const { items, renderItem, filter, renderEmpty } = props;
 
   if (!items) {
     console.error("ArrayRender: items is null");
     return null;
+  }
+
+  if (items.length === 0) {
+    return renderEmpty ? renderEmpty() : null;
   }
 
   return (
